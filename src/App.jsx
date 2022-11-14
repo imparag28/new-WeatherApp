c637884d6dbe97507e04c23ff4e430066426a453
parag.sol <parag.sol@cars24.com>
2022-11-14 19:16:25 +0530

    initail setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import { Button } from '@mui/material'
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <h1>hiii</h1>
+
+    </div>
+  )
+}
+
+export default App
